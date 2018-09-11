574c169a7c290eb5afd7c6facbeeeb4e61a27db6
Clarice <claricemayhilton@gmail.com>
2018-09-11 15:22:34 +0100

    hover text, new background

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 
+/*$(".inner-circle").mouseover(function(e){
+$("."+this.id).show();
+$("."+this.id).css({
+top: (e.pageY ) + "px",
+left: (e.pageX ) + "px"
+});
+});
+$(".showPopUp").mouseout(function(){
+$("."+this.id).hide();
+});*/
+
+jQuery(document).ready(function() {
+    jQuery('.inner-circle').hover(function() {
+        jQuery(this).find('div:second').show()
+    }, function() {
+        jQuery(this).find('div:second').hide();
+    });
+});
